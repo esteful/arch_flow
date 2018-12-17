9fbecbd7ea02735f7eff64da9fcffd21a51dc0c9
Esteful <estefaniacalparsoro@gmail.com>
2018-12-17 20:03:26 +0100

    add more than 6 shapes in for PCA visualization

--- R_Fun/arch_PCA.R
+++ R_Fun/arch_PCA.R
@@ -22,7 +22,7 @@ for (i in nplot){
                          colour = colnames(df_raw[i]),  #set the categories to be displayed  
                          label = labels,                #TRUE if the name of the ANIDs are to be displayed
                          label.size = 3,                 #set the label sizes (not shapes)
-                         
+                                            
                          #shapes      
                          shape = colnames(df_raw)[shape_cat_number],        #set the category to display by shapes, or chose from 1-25 shapes to display all equally, 15 by default       
                          size = 1,                        #set the size of the shapes
@@ -35,8 +35,15 @@ for (i in nplot){
                          
                          #eLlipse   
                          frame = frame,   #true by default, if "norm" is removed the shaope of the frame is geometrical
-                         frame.type = "norm") + ggtitle(label = "PCA") + theme(plot.title = element_text(hjust = 0.5)) + labs(caption= paste("PCA using:", paste(noquote(colnames(df_chem)), collapse = ", ")))+ theme(plot.caption = element_text(size=7, hjust=0.5, margin=margin(t=1))))
-    
+                         frame.type = "norm") 
+                         
+            
+                 + scale_shape_manual(values=c(1:15))
+                 + ggtitle(label = "PCA") 
+                 + theme(plot.title = element_text(hjust = 0.5))  
+                 + labs(caption= paste("PCA using:", paste(noquote(colnames(df_chem)), collapse = ", "))) 
+                 + theme(plot.caption = element_text(size=7, hjust=0.5, margin=margin(t=1))))
+   
     
     
     plot(eval(parse(text =paste("pca_cat", i, sep = "_")))) #plot PCAs according to npca vector
