1bc4717ac823b0e3485e9e694bf6b876d0124893
esteful <estefaniacalparsoro@gmail.com>
2020-04-28 13:00:08 +0200

    updates

--- R_Fun/install.R
+++ R_Fun/install.R
@@ -1,4 +1,10 @@
 #All the packages required to run ArchFlow
+
+#First of all, make sure the working directory is the correct one. The same as the .Rproj file.
+getwd() -> current_wd
+setwd(current_wd)
+
+
 packages <- c("archdata",
                       "plyr",
                       "dplyr",
@@ -23,30 +29,32 @@ packages <- c("archdata",
 
 #All the functions used to run ArchFLow
                        
-            source("../../R_Fun/arch_dendro.R")
-            source("../../R_Fun/arch_latex.R")
-            source("../../R_Fun/arch_logcenter.R")
-            source("../../R_Fun/arch_PCA.R")
-            source("../../R_Fun/arch_pie_plot.R")
-            source("../../R_Fun/arch_sum_table.R")
-            source("../../R_Fun/arch_triangles.R")
-            source("../../R_Fun/arch_evenness.R")
-            source("../../R_Fun/arch_varmat.R")
-            source("../../R_Fun/classifica.R")
-            source("../../R_Fun/entropia.R") 
-            source("../../R_Fun/etiquetes_elements.R")
-            source("../../R_Fun/arch_heatmap.R")
-            source("../../R_Fun/arch_scatter_matrix.R")
+            source("R_Fun/arch_latex.R")
+            source("R_Fun/arch_logcenter.R")
+            source("R_Fun/arch_PCA.R")
+            source("R_Fun/arch_pie_plot.R")
+            source("R_Fun/arch_sum_table.R")
+            source("R_Fun/arch_triangles.R")
+            source("R_Fun/arch_evenness.R")
+            source("R_Fun/arch_varmat.R")
+            source("R_Fun/classifica.R")
+            source("R_Fun/entropia.R") 
+            source("R_Fun/etiquetes_elements.R")
+            source("R_Fun/arch_heatmap.R")
+            source("R_Fun/arch_scatter_matrix.R")
             
             rm(new)
             rm(packages)
             
   
-###BUGS: WHEN DOING STARGAZER FOR LATEX THE DIGITS ARE NOT ROUNDED IN RSD!!!
+
+
+message("The following folder is the working directory of this project:")
+print(getwd())
+
 message("#All the packages were installed and loaded succesfull! #All the functions were loaded to the system succesfully! # The ArchFlow rmd files are ready to Run!")
 
-
-
+#sessionInfo() 
 
 
 
