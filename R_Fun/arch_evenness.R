d7da0f237a768013b16d9c92575dff2a63dc894f
esteful <estefaniacalparsoro@gmail.com>
2021-03-25 18:03:57 +0100

    adjust automatically the labels of MVC
    Depending on the number of row the value required for x_dodge should be different

--- R_Fun/arch_evenness.R
+++ R_Fun/arch_evenness.R
@@ -1,14 +1,8 @@
-
-
-
-
-
-
 "arch_evenness" <- function(df_chem)
   
 {
   #It serves to asses the evenness of a dataset and outputs tree plots:
-  #Evennes plot, MVC dendrogram and CoDa Dendrogram. 
+  #Evenness plot, MVC dendrogram and CoDa Dendrogram. 
   #Code developed from the original code from J.Buxeda i Garrigos.
   #Based on the observations from Aitchison and Buxeda on compositional data.
   #depends on following packages: compositions, plotrix, ggplot2, ggthemes and devEMF
@@ -24,269 +18,265 @@
   #A Festchrift in Honor to Dr. Edward Sayre, March, 185–198.
   
   
-  ##create the matrix with the corresponding dimensions
+  # number of columns
+  n_variables <- ncol(df_chem) 
   
-    # number of columns
-    n_variables <- ncol(df_chem) 
-    
-    # create a matrix with column number and equal row number
-    varmat <- matrix(0, n_variables, n_variables) 
-   
-    # same matrix with 4 more rows 
-    varmat2<-matrix(0, n_variables + 4, n_variables) 
-    
-    
-    
-  # Fill the matrix with values
-    
-    # calculate the diagonal matrix with the variances of the 
-    # log x divided by the value of the corresponding variable
-    
-    for(i in 1:n_variables) {
-      varmat[, i] <- 
+  # create a matrix with column number and equal row number
+  varmat <- matrix(0, n_variables, n_variables) 
+  
+  # same matrix with 4 more rows 
+  varmat2<-matrix(0, n_variables + 4, n_variables) 
+  
+  #Fill the matrix with values
+  
+  # calculate the diagonal matrix with the variances of the 
+  # log x divided by the value of the corresponding variable
+  for(i in 1:n_variables) {
+    varmat[, i] <- 
       diag(var(log(df_chem/df_chem[, i]))) 
-    }
-    
-  
+  }
   
   #calculate total variation
-    totvar <- sum(varmat)/(2 * n_variables)
+  totvar <- sum(varmat)/(2 * n_variables)
   
   #"t.i": the sum of the individual variabilities for each value in a given column
-    varsum  <- apply(varmat, 2, sum)   
+  varsum  <- apply(varmat, 2, sum)   
   
   # vt/t.i (ratio of total variation and individual variation)
-    varprop <- totvar/varsum      
+  varprop <- totvar/varsum      
   
   #"r v,t" calculate the correlation between individual and total variation
-    varcor  <- vector(mode="numeric", length= n_variables)    
+  varcor  <- vector(mode="numeric", length= n_variables)    
   
   
   #Correlation vector
   
-    for(i in 1:n_variables) {
-      varcor[i] <- 
+  for(i in 1:n_variables) {
+    varcor[i] <- 
       cor(varmat[-c(i),i], varsum[-i])
-      }
+  }
   
-  
-  #add values to the matrix #falta mirar si aquí no hace falta {}
-    for(i in 1:n_variables) 
+  #add values to the empty matrix 
+  for(i in 1:n_variables)
+  { 
     varmat2[i,]  <-  varmat[i,]  #add previously calculated values
     varmat2[n_variables+1,]<-  varsum      #add the sum of all variabilities 
     varmat2[n_variables+2,]<-  varprop     #add the ratio of total variation and individual variation
     varmat2[n_variables+3,]<-  varcor      #add correlation values  
     varmat2[n_variables+4,1]<- totvar      #add the total variation, only one value in the first column
-    
-    
+  }
+  
   #set the names
-    dimnames(varmat2) <-
+  dimnames(varmat2) <-
     list(c(dimnames(df_chem)[[2]],"t.i","vt/t.i","r v,t","vt"),c(dimnames(df_chem)[[2]])) 
   
   #vt/t.i values by order
-    ord <- order(varprop)
-    colnames(df_chem)[ord[n_variables]] -> lvar #get the name of the variable
-    
+  ord <- order(varprop)
+  colnames(df_chem)[ord[n_variables]] -> lvar #get the name of the less varying variable
   
   #set names to show after in the MVC plot
-    names(varsum) <-  colnames(df_chem) #
-    varsum[order(varsum, decreasing = T)] -> varsum_ordered_vec
-    as.data.frame(varsum_ordered_vec) -> df_varsum
-    
+  names(varsum) <-  colnames(df_chem) #
+  varsum[order(varsum, decreasing = T)] -> varsum_ordered_vec
+  as.data.frame(varsum_ordered_vec) -> df_varsum
   
-  ##For MVC plot
-    for (i in 1:length(row.names(df_varsum))) {
-      if (regexpr ("^Fe2O3$",as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("Fe"["2"]*"O"["3"])
-      if (regexpr ("^Al2O3$", as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("Al"["2"]*"O"["3"])
-      if (regexpr ("^P2O5$", as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("P"["2"]*"O"["5"])
-      if (regexpr ("^TiO2$", as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("TiO"["2"])
-      if (regexpr ("^Na2O$", as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("Na"["2"]*"O")
-      if (regexpr ("^K2O$", as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("K"["2"]*"O")
-      if (regexpr ("^SiO2$", as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("SiO"["2"])
-    }
+  ##For MVC plot (set oxide names when these are in the dataset)
+  for (i in 1:length(row.names(df_varsum))) {
+    if (regexpr ("^Fe2O3$",as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("Fe"["2"]*"O"["3"])
+    if (regexpr ("^Al2O3$", as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("Al"["2"]*"O"["3"])
+    if (regexpr ("^P2O5$", as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("P"["2"]*"O"["5"])
+    if (regexpr ("^TiO2$", as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("TiO"["2"])
+    if (regexpr ("^Na2O$", as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("Na"["2"]*"O")
+    if (regexpr ("^K2O$", as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("K"["2"]*"O")
+    if (regexpr ("^SiO2$", as.character(row.names(df_varsum)[i]))==T) row.names(df_varsum)[i]<-expression("SiO"["2"])
+  }
   
   row.names(df_varsum) <- sub(pattern = "Fe2O3",replacement =  expression("Fe"["2"]*"O"["3"]), x=row.names(df_varsum))
   
   
-  ##FOR MVC plot and its cuts, and also MVC dendro
-    pop <-dim(df_chem)[2]+1            # all variables + 1
-    mat <- matrix(0, pop,1)       # a matrix with all variables + 1 
-
+  
+  
+  
+  
+  ##For MVC plot and its cuts, and also MVC dendro
+  n_varplus1 <-n_variables + 1            # all variables + 1
+  mat <- matrix(0, n_varplus1,1)       # a matrix with all variables + 1 
+  
   #Create a dataframe
-    MVC<-as.data.frame(varmat2)               #MVC (Compositional variation matrix) varmat2 previously created
-    tMVC<-as.data.frame(t(MVC))               #transpose the matrix
-    ordre<-order(tMVC[,c(pop)],decreasing=T)  #order the variables 
-    tMVC<-as.data.frame(tMVC[ordre,])         #save the matrix ordered
-    
-    
-  #MVC plot (external function called "entropia" used)
-    for(i in 1:(pop-1)) {
-      mat[i,1] <- tMVC[i, pop]
-    }
-    
-    mat[pop,1] <- MVC[pop+3,1]
-    dimnames(mat)[[1]]<-c(dimnames(tMVC)[[1]], "vt")
-    tmat<-t(mat[c(1:pop-1),])
-    tmatentro<-entropia02(tmat-mat[pop,1]) #entropia function
-    #par(mar=c(5,5,4,2)+0.1,mgp=c(3,1,0))
+  MVC<-as.data.frame(varmat2)               #convert varmat2 matrix to data frame
+  tMVC<-as.data.frame(t(MVC))               #transpose the dataframe
+  
+  
+  ordered_varsum_vec<-order(varsum, decreasing=T) #order the variables according to the varsum decreasingly and get
+  
+  tMVC<-as.data.frame(tMVC[ordered_varsum_vec,])  #save the matrix ordered as dataframe
+  
+  
+  #MVC plot (uses external function called "entropia" used)
+  for(i in 1:(n_variables)) {
+    mat[i,1] <- tMVC[i, n_varplus1]
+  }
+  
+  mat[n_varplus1,1] <- MVC[n_varplus1+3,1]  #total variation (vt)
+  dimnames(mat)[[1]]<-c(dimnames(tMVC)[[1]], "vt")
+  
+  tmat<-t(mat[c(1:n_varplus1-1),])
+  tmatentro<-entropia02(tmat-mat[n_varplus1,1]) #entropia function
   
   
   #calculate values 
-    h2<-round(tmatentro$Entropy[1,pop],dig=2)
-    h2p<- round(tmatentro$Entropy[1,pop+1]*100,dig=2)
-    vt<-round(mat[pop,1],dig=2)
-    
+  h2<-round(tmatentro$Entropy[1,n_varplus1],dig=2)
+  h2p<- round(tmatentro$Entropy[1,n_varplus1+1]*100,dig=2)
+  vt<-round(mat[n_varplus1,1],dig=2)
+  
   #add doted lines 
-    cuts=c(0.3, 0.5, 0.9) #vertical doted axis 
-    vec3<- length(cuts)            # 3
-    matrix3<- matrix(0,vec3,1)         # (0,3,1)
-    
-    for (i in 1:vec3) {
-      matrix3[i,1] <- 
-      length(which(MVC[pop+1,]<cuts[i])) #matrix with n observation below cuts
-    }
-    
- 
+  cuts=c(0.3, 0.5, 0.9) #vertical doted axis 
+  vec3<- length(cuts)            # 3
+  matrix3<- matrix(0,vec3,1)     # (0,3,1)
+  
+  for (i in 1:vec3) {
+    matrix3[i,1] <- 
+      length(which(MVC[n_varplus1+1,]<cuts[i])) #matrix with n observation below cuts
+  }
   
   
-  ##CREATE THE PLOT
   
-    MVC_plot <- 
-      ggplot(df_varsum, aes(x=row.names(df_varsum), y=varsum_ordered_vec)) +  #especify data to plot 
-      geom_line(aes(group=1)) + #add the line between dots
-      geom_point(size=3, shape=20) +  #add the dots (change size or shape)
-      scale_x_discrete(limits = c(row.names(df_varsum)), labels = NULL) +#avoid alphabetical order of elements
-      expand_limits(x = 43) + #add space between the right bar and the las element label
-      theme_few(base_size = 7, base_family = "sans") + #change theme 
-      theme(plot.margin = unit(c(1, 1, 1, 1), "cm"), text = element_text(size=rel(4))) +
-      labs(y=expression(tau[.i]), x = NULL) + # add X label
-      geom_text(
-        data = df_varsum,
-        label = row.names(df_varsum),
-        parse = TRUE,
-        nudge_x = 0.5,
-        nudge_y = 5,
-        check_overlap = FALSE,
-        na.rm = FALSE,
-        show.legend = NA,
-        inherit.aes = TRUE, 
-        angle = 69
-      ) +
-      
-      theme(plot.title = element_text(hjust = 0.5, size = rel(4)))+  #Graphic Title
-      ggtitle(paste0("Data (n = ", nrow(df_chem),")", collapse = NULL))+  #paste0 removes the space
-      
-      labs(caption =  bquote(""*"H"[2]*" = "*.(h2)*" Sh, " ~ "H"[2]*" % = "*.(h2p) ~ "     "*"vt = "*.(vt))) +
-      theme(plot.caption = element_text(size = 12)) +
-      
-      #annotate("text", 
-      #              x= nrow(df_varsum)*0.6, 
-      #              y= max(df_varsum$varsum_ordered_vec*0.85),
-      #              na.rm = FALSE,
-      #              label= bquote("H"[2]*" = "*.(h2)*"Sh"~ "H"[2]*" % = "*.(h2p) ~ "vt = "*.(vt))) 
-      
-      
-      
-      #0.3   
-      annotate("segment", x = matrix3[1]+0.5, xend = matrix3[1]+0.5, y = 0, yend = max(varsum_ordered_vec)/2, 
-               linetype="dashed", colour = "black", size =0.1) +
-      annotate("text", x = matrix3[1]+0.5, y = 0, label = "0.3")+ 
-      #0.5
-      annotate("segment", x = matrix3[2]+0.5, xend = matrix3[2]+0.5, y = 0, yend = max(varsum_ordered_vec)/2, 
-               linetype="dashed", colour = "black", size =0.1) +
-      annotate("text", x = matrix3[2]+0.5, y = 0, label = "0.5") +
-      #0.9
-      annotate("segment", x = matrix3[3]+0.5, xend = matrix3[3]+0.5, y = 0, yend = max(varsum_ordered_vec)/2, 
-               linetype="dashed", colour = "black", size =0.1) +
-      annotate("text", x = matrix3[3]+0.5, y = 0, label = "0.9") +
-      
-      annotate("segment", x = 0, xend =dim(df_varsum)[1], y = min(df_varsum), yend = min(df_varsum), 
-               linetype="dashed", colour = "black", size =0.1) 
-    
-    
   
-    
-    #save the plot
-    ggsave("MVC_plot.pdf",MVC_plot)
-    
-    plot(MVC_plot)
-    
+  ##CREATE THE MVC PLOT
   
-  #Dendrogram of MVC
+  MVC_plot <- 
+    ggplot(df_varsum, aes(x=row.names(df_varsum), y=varsum_ordered_vec)) +  #especify data to plot 
+    geom_line(aes(group=1)) + #add the line between dots
+    geom_point(size=3, shape=20) +  #add the dots (change size or shape)
+    scale_x_discrete(limits = c(row.names(df_varsum)), labels = NULL) +#avoid alphabetical order of elements
+    theme_few(base_size = 7, base_family = "sans") + #change theme 
+    theme(plot.margin = unit(c(1, 1, 1, 1), "cm"), text = element_text(size=rel(4))) +
+    labs(y=expression(tau[.i]), x = NULL) + # add X label
+    #expand_limits(x = 30) + #add space between the right bar and the last element (label)
+    geom_text(
+      data = df_varsum,
+      label = row.names(df_varsum),
+      parse = TRUE,
+      #nudge_x = 0.5, #change this for label position in x axis  
+      nudge_y = (ifelse(test = nrow(df_chem)>20, #change this for label position in y axis
+                        yes = ncol(df_chem)*0.03, 
+                        no = 0.05)),  #if more than 20 individuals change the label position
+      check_overlap = FALSE,
+      na.rm = FALSE,
+      show.legend = NA,
+      inherit.aes = TRUE, 
+      angle = 69 #change the angle of the labels
+    ) +
+    
+    theme(plot.title = element_text(hjust = 0.5, size = rel(4)))+  #Graphic title
+    ggtitle(paste0("Data (n = ", nrow(df_chem),")", collapse = NULL))+  #paste0 removes the space
+    
+    labs(caption =  bquote(""*"H"[2]*" = "*.(h2)*" Sh, " ~ "H"[2]*" % = "*.(h2p) ~ "     "*"vt = "*.(vt))) +
+    theme(plot.caption = element_text(size = 12)) +
+    
+    
+    ##Add data and dotted lines to the MVC plot
+    
+    #0.3   
+    annotate("segment", x = matrix3[1]+0.5, xend = matrix3[1]+0.5, y = 0, yend = max(varsum_ordered_vec)/2, 
+             linetype="dashed", colour = "black", size =0.1) +
+    annotate("text", x = matrix3[1]+0.5, y = 0, label = "0.3")+ 
+    #0.5
+    annotate("segment", x = matrix3[2]+0.5, xend = matrix3[2]+0.5, y = 0, yend = max(varsum_ordered_vec)/2, 
+             linetype="dashed", colour = "black", size =0.1) +
+    annotate("text", x = matrix3[2]+0.5, y = 0, label = "0.5") +
+    #0.9
+    annotate("segment", x = matrix3[3]+0.5, xend = matrix3[3]+0.5, y = 0, yend = max(varsum_ordered_vec)/2, 
+             linetype="dashed", colour = "black", size =0.1) +
+    annotate("text", x = matrix3[3]+0.5, y = 0, label = "0.9") +
+    
+    annotate("segment", x = 0, xend =dim(df_varsum)[1], y = min(df_varsum), yend = min(df_varsum), 
+             linetype="dashed", colour = "black", size =0.1) 
   
-    dendroMVC<-hclust(as.dist(MVC[c(1:(dim(MVC)[2])),]),"ave") #hclust with average method
-    
-    for (i in 1:(dim(MVC)[2])) {
-      if (regexpr ("^Fe2O3$",as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("Fe"["2"]*"O"["3"])
-      if (regexpr ("^Al2O3$", as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("Al"["2"]*"O"["3"])
-      if (regexpr ("^P2O5$", as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("P"["2"]*"O"["5"])
-      if (regexpr ("^TiO2$", as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("TiO"["2"])
-      if (regexpr ("^Na2O$", as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("Na"["2"]*"O")
-      if (regexpr ("^K2O$", as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("K"["2"]*"O")
-      if (regexpr ("^SiO2$", as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("SiO"["2"])
-    }
-    
-    #plot
-    plot(as.dendrogram(dendroMVC), main= "MVC Dendrogram")
-    
-    
-    
-    #save the plot as pdf
-    MVCdendro <-recordPlot(dendroMVC)
-    pdf("MVC_Dendrogram.pdf")
-    replayPlot(MVCdendro)
-    dev.off()
-    
+  
+  #save the plot
+  ggsave("MVC_plot.pdf",MVC_plot)
+  
+  #visualize the plot
+  plot(MVC_plot)
+  
+  
+  
+  ############################    2 DENDROGRAM OF MVC  ######################################################    
+  
+  dendroMVC<-hclust(as.dist(MVC[c(1:(dim(MVC)[2])),]),"ave") #hclust with average method
+  
+  for (i in 1:(dim(MVC)[2])) {
+    if (regexpr ("^Fe2O3$",as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("Fe"["2"]*"O"["3"])
+    if (regexpr ("^Al2O3$", as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("Al"["2"]*"O"["3"])
+    if (regexpr ("^P2O5$", as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("P"["2"]*"O"["5"])
+    if (regexpr ("^TiO2$", as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("TiO"["2"])
+    if (regexpr ("^Na2O$", as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("Na"["2"]*"O")
+    if (regexpr ("^K2O$", as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("K"["2"]*"O")
+    if (regexpr ("^SiO2$", as.character(dendroMVC[[4]][i]))==T) dendroMVC[[4]][i]<-expression("SiO"["2"])
+  }
+  
+  #plot
+  plot(as.dendrogram(dendroMVC), main= "MVC Dendrogram")
+  
+  #save the plot as pdf
+  MVCdendro <-recordPlot(dendroMVC)
+  pdf("MVC_Dendrogram.pdf")
+  replayPlot(MVCdendro)
+  dev.off()
+  
+  
+  ##############################################################################################################    
+  
+  
+  
+  ############################    3. CoDadendrogram   #######################################################    
   
   
   ##CoDadendrogram
+  
+  mida.boxplot= 30 #used in codadendro
+  rang.boxplot=c(-4,4) #used in codadendro
+  
+  
+  classi<-classifica(dendroMVC) #classifica function is external
+  Signary<-as.data.frame(t(classi$signary))
+  
+  dimnames(Signary)[[1]]<-dimnames(df_chem)[[2]]
+  
+  CoDaDendrogram(
+    acomp(df_chem),
+    signary=Signary, 
+    border="red4", 
+    col="goldenrod3", 
+    type="boxplot", 
+    box.space=mida.boxplot, 
+    range=rang.boxplot)
+  
+  
+  
+  
+  #save the plot of CoDaDendrogram
+  CodaDendroPlot<-recordPlot()
+  MVCdendro <-recordPlot(CodaDendroPlot)
+  pdf("CoDaDendrogram.pdf")
+  replayPlot(CodaDendroPlot)
+  dev.off()
+  
+  #To save the nummeric data
+  bases <- gsi.buildilrBase(Signary)
+  Fitxer_ilr <- ilr(df_chem,bases)
+  par(mar=c(5,4,4,2)+0.1,mgp=c(3,1,0))
+  list(MVC=MVC,
+       Entropia=tmatentro$Entropia,
+       Probabilitat= tmatentro$Probabilitat, 
+       Signary= Signary, 
+       Bases= bases, 
+       Fitxer_ilr= Fitxer_ilr)
+  
+  
+  ####################################################################################################    
+  #Add the less varying element (lvar) to the global environment. 
+  assign("lvar", lvar,.GlobalEnv)
+  
+}
 
-    mida.boxplot= 30 #used in codadendro
-    rang.boxplot=c(-4,4) #used in codadendro
-  
-    
-    classi<-classifica(dendroMVC)
-    Signary<-as.data.frame(t(classi$signary))
-    
-    dimnames(Signary)[[1]]<-dimnames(df_chem)[[2]]
-   
-    CoDaDendrogram(
-      acomp(df_chem),
-        signary=Signary, 
-        border="red4", 
-        col="goldenrod3", 
-        type="boxplot", 
-        box.space=mida.boxplot, 
-        range=rang.boxplot)
-    
-    
-    
-    
-    #save the plot of CoDaDendrogram
-    CodaDendroPlot<-recordPlot()
-    MVCdendro <-recordPlot(CodaDendroPlot)
-    pdf("CoDaDendrogram.pdf")
-    replayPlot(CodaDendroPlot)
-    dev.off()
-    
-  
-  
-  ###MODIFICADO: 
-  
-    bases <- gsi.buildilrBase(Signary)
-    Fitxer_ilr <- ilr(df_chem,bases)
-    par(mar=c(5,4,4,2)+0.1,mgp=c(3,1,0))
-    list(MVC=MVC,
-         Entropia=tmatentro$Entropia,
-         Probabilitat= tmatentro$Probabilitat, 
-         Signary= Signary, 
-         Bases= bases, 
-         Fitxer_ilr= Fitxer_ilr)
-    
-    
-    
-    #Add the less varying element (lvar) to the global environment. 
-    assign("lvar", lvar,.GlobalEnv)
-    
-    
-  }
